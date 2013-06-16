cbcb67094a51e7cfaf677c4ba29f575da1b06260
Roman Kurbatskyi <r.kurbatskyi@gmail.com>
2013-06-16 15:40:20 +0200

    added authenticator

---
+++ app/authenticated/Secured.java
@@ -0,0 +1,14 @@
+package authenticated;
+
+import controllers.routes;
+import play.mvc.Http.Context;
+import play.mvc.Result;
+import play.mvc.Security;
+
+public class Secured extends Security.Authenticator {
+
+    @Override
+    public Result onUnauthorized(Context ctx) {
+        return redirect(routes.Application.signin());
+    }
+}
