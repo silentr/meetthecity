1fcb956e0a9fb458fe1015b8c99de6234d6d5643
Roman Kurbatskyi <r.kurbatskyi@gmail.com>
2013-06-16 17:21:02 +0200

    added signup route, formatted signup view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,7 @@ import models.Review;
 import models.Tour;
 import models.User;
 import models.form.Login;
+import models.form.SignUp;
 import play.Logger;
 import play.api.templates.Html;
 import play.data.Form;
@@ -14,12 +15,7 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import scala.collection.mutable.StringBuilder;
-import views.html.index;
-import views.html.joinatour;
-import views.html.signin;
-import views.html.tours;
-import views.html.user;
-
+import views.html.*;
 import com.avaje.ebean.Ebean;
 
 public class Application extends Controller {
@@ -27,16 +23,16 @@ public class Application extends Controller {
     public static Result authenticate() {
         Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
         if (loginForm.hasErrors()) {
-            
+
             Logger.debug("User didn't logged in");
-            
+
             return badRequest(signin.render(loginForm));
         } else {
             session().clear();
             session("username", loginForm.get().username);
-            
+
             Logger.debug("User " + loginForm.get().username + " logged in");
-            
+
             return redirect(routes.Application.index());
         }
     }
@@ -48,20 +44,24 @@ public class Application extends Controller {
     public static Result signin() {
         return ok(signin.render(Form.form(Login.class)));
     }
-    
+
+    public static Result signup() {
+        return ok(signup.render(Form.form(SignUp.class)));
+    }
+
     public static Result tours() {
 
         List<Tour> tourList = Tour.find.all();
         return ok(tours.render(tourList));
     }
-    
+
     public static Result createUser() {
         User user = Form.form(User.class).bindFromRequest().get();
         user.save();
 
         return redirect(routes.Application.index());
     }
-    
+
     public static Result addUser() {
         return ok(user.render());
     }
@@ -77,20 +77,20 @@ public class Application extends Controller {
 
         return user != null ? ok(Json.toJson(user)) : notFound();
     }
-    
+
     public static Result joinATour(String id) {
 
         Logger.info(id);
-		Tour tour = new Model.Finder<String, Tour>(String.class, Tour.class).byId("1");
-		return ok(joinatour.render(tour));
-	}
-    
+        Tour tour = new Model.Finder<String, Tour>(String.class, Tour.class).byId("1");
+        return ok(joinatour.render(tour));
+    }
+
     public static Result joinATourSubmit() {
         String tourId = Form.form().bindFromRequest().get("id");
         Ebean.beginTransaction();
         Tour tour = Tour.find.byId(Long.valueOf(tourId));
         String username = session("connected");
-        if(username != null){
+        if (username != null) {
             User user = User.find.byId(username);
             tour.tourists.add(user);
             user.tours.add(tour);
@@ -98,14 +98,14 @@ public class Application extends Controller {
             user.update();
             Ebean.commitTransaction();
             return ok();
-        }else{
+        } else {
             Ebean.endTransaction();
             return unauthorized("Oops, you are not connected");
         }
     }
-    
-    public static Result getAllReviews(){
-    	List<Review> reviews = Review.find.all();
-    	return ok(Json.toJson(reviews));
+
+    public static Result getAllReviews() {
+        List<Review> reviews = Review.find.all();
+        return ok(Json.toJson(reviews));
     }
 }
