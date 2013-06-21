06eeea30a56f1c07dc0fcfb6a6f4a0dc360dc2d3
Roman Kurbatskyi <r.kurbatskyi@gmail.com>
2013-06-21 14:54:47 +0200

    deleted unused imports

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,31 +1,14 @@
 package controllers;
 
-import static play.data.Form.form;
-
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
-import models.Location;
-import models.Tour;
 import models.User;
-import models.form.TourForm;
-import play.Logger;
 import play.api.templates.Html;
-import play.data.Form;
 import play.libs.Json;
 import play.mvc.Controller;
-import play.mvc.Http.MultipartFormData;
-import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
-import play.mvc.Security;
 import scala.collection.mutable.StringBuilder;
-import validators.Secured;
-import views.html.createatour;
 import views.html.index;
-import views.html.tours;
-import views.html.viewatour;
-import views.html.helper.form;
 
 public class Application extends Controller {
 
